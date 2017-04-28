7061cfa61a821f04c7bc578c0e78dc929ccfa34c
Maartje de Jonge <maartjedejonge@gmail.com>
2017-04-28 11:42:16 +0000

    pass topic argument

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@
 Usage:
   main.py <path> <path>
   
-  First argument (optional) is the path to the directory (or file) containing the hotel reviews in json format.
-  Second argument (optional) is the path to the file containing the semantics in json format.
+  First argument is the path to the directory (or file) containing the hotel reviews in json format.
+  Second argument (optional) is the topic.
+  Third argument (optional) is the path to the file containing the semantics in json format.
 """
 
 import sys
@@ -12,7 +13,7 @@ from load import load_json
 from process import get_weights, get_intensifiers, get_reviews, get_hotel
 
 
-def main(reviews = "data/reviews3.json", semantics = "semantics/semantics.json"):
+def main(reviews, topic=None, semantics = "semantics/semantics.json"):  
     semantics_data = load_json(semantics)
     df_weights = get_weights(semantics_data)
     df_intensifiers = get_intensifiers(semantics_data)
@@ -20,13 +21,16 @@ def main(reviews = "data/reviews3.json", semantics = "semantics/semantics.json")
     review_data = load_json(reviews) #TODO: support passing dir with review files
     df_reviews = get_reviews(review_data)
     hotel = get_hotel(review_data)
+    
 
-if __name__ == "__main__":
+if __name__ == "__main__": #TODO: topic can be a list with synonyms. 
+    if len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
     if len(sys.argv) == 3:
         main(sys.argv[1], sys.argv[2])
     if len(sys.argv) == 2:
         main(sys.argv[1])
     if len(sys.argv) == 1:
-        main()
+        main("data/reviews3.json", "room") #TODO make path to reviews required? 
     else:
         raise ValueError('Wrong number of arguments. Arguments are: path to reviews directory (optional) and path to semantics file (optional)')
