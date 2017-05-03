cb8dd64d980262daa4df985074eac00103edd969
Maartje de Jonge <maartjedejonge@gmail.com>
2017-05-03 21:28:01 +0000

    add unit test for sentiment scorer

--- test/test_sentiment_scorer.py
+++ test/test_sentiment_scorer.py
@@ -6,10 +6,6 @@ class TestSentimentScorer(unittest.TestCase):
     def setUp(self):
         sentiment_weights = {
             "great" : 2,
-            "uncomfortable" : -1,
-            "didn't work" : -1,
-            "old" : -1, 
-            "hard" : -1,
             "bad" : -2
         } 
         multiplier_weights = {
@@ -19,10 +15,22 @@ class TestSentimentScorer(unittest.TestCase):
         self._sentiment_scorer = SentimentScorer()
         self._sentiment_scorer.initialize(sentiment_weights, multiplier_weights)
 
-    def test_xx(self):
+    def test_score_sentiment_phrase(self):
         sentiment_blocks = [([], "great")]
         actual = self._sentiment_scorer.score_sentiment(sentiment_blocks)
-        expected = 2
+        expected = 2.0
+        self.assertEqual(actual, expected)
+
+    def test_score_sentiment_phrase_single_multiplier(self):
+        sentiment_blocks = [(["really"], "great")]
+        actual = self._sentiment_scorer.score_sentiment(sentiment_blocks)
+        expected = 3.0
+        self.assertEqual(actual, expected)
+
+    def test_score_sentiment_phrase_multiple_multipliers(self):
+        sentiment_blocks = [(["not", "really"], "great")]
+        actual = self._sentiment_scorer.score_sentiment(sentiment_blocks)
+        expected = -3.0
         self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
