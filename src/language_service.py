e1324592ba225d757fd15cff53069ec3234fc7ea
Maartje de Jonge <maartjedejonge@gmail.com>
2017-05-04 07:29:08 +0000

    make language service a class

--- src/language_service.py
+++ src/language_service.py
@@ -1,29 +1,38 @@
-# TODO: use some dictionairy API
-def get_singular_or_plural(word):
-    """ Returns singular or plural form (or None)
-    """
-    
-    if word in ["personnel", "staff"]:
-        return None
-    if word[-1:] == "y":
-        return word[:-1] + "ies"
-    if word[-3:] == "ies":
-        return word[:-3] + "y"
-    if word[-1:] == "s":
-        return word[:-1] 
-    else:
-        return word + "s" 
+class LanguageService:
 
-synonyms = {
-    "staff" : ["personnel"],
-    "personnel" : ["staff"],
-    "lavatory" : ["toilet"],
-    "toilet" : ["lavatory"]
-}
+    def __init__(self):
+        # for illustration purpose only
+        self._synonyms = {
+            "staff" : ["personnel"],
+            "personnel" : ["staff"],
+            "lavatory" : ["toilet"],
+            "toilet" : ["lavatory"]
+        }
+        self._plural_only_forms = [
+            "personnel", 
+            "staff"
+        ]
 
-# TODO: use some dictionairy API
-def get_synonyms(word):
-    """ Returns a list of synonyms
-    """
-    return synonyms.get(word, [])
+
+    # TODO: use some dictionairy API
+    def get_singular_or_plural(self, word):
+        """ Returns singular or plural form (or None)
+        """
+        if word in self._plural_only_forms:
+            return None
+        if word[-1:] == "y":
+            return word[:-1] + "ies"
+        if word[-3:] == "ies":
+            return word[:-3] + "y"
+        if word[-1:] == "s":
+            return word[:-1] 
+        else:
+            return word + "s" 
     
+    
+    # TODO: use some dictionairy API
+    def get_synonyms(self, word):
+        """ Returns a list of synonyms
+        """
+        return self._synonyms.get(word, [])
+        
--- test/test_language_service.py
+++ test/test_language_service.py
@@ -1,46 +1,50 @@
-from src.language_service import get_singular_or_plural, get_synonyms
+from src.language_service import LanguageService
+
 import unittest
 
 class TestLanguageService(unittest.TestCase):
 
+    def setUp(self):
+        self._language_service = LanguageService()
+
     def test_get_singular_or_plural_returns_plural_by_adding_s(self):
         word = "room"
-        actual = get_singular_or_plural(word)
+        actual = self._language_service.get_singular_or_plural(word)
         expected = "rooms"
         self.assertEqual(actual, expected)
 
     def test_get_singular_or_plural_returns_singular_by_removing_s(self):
         word = "rooms"
-        actual = get_singular_or_plural(word)
+        actual = self._language_service.get_singular_or_plural(word)
         expected = "room"
         self.assertEqual(actual, expected)
 
     def test_get_singular_or_plural_returns_plural_for_y(self):
         word = "baby"
-        actual = get_singular_or_plural(word)
+        actual = self._language_service.get_singular_or_plural(word)
         expected = "babies"
         self.assertEqual(actual, expected)
 
     def test_get_singular_or_plural_returns_singular_for_ies(self):
         word = "facilities"
-        actual = get_singular_or_plural(word)
+        actual = self._language_service.get_singular_or_plural(word)
         expected = "facility"
         self.assertEqual(actual, expected)
 
     def test_get_singular_or_plural_returns_none_for_plural_only_terms(self):
         word = "staff"
-        actual = get_singular_or_plural(word)
+        actual = self._language_service.get_singular_or_plural(word)
         expected = None
         self.assertEqual(actual, expected)
 
     def test_get_synonyms_returns_synonyms_when_synonyms_found(self):
         word = "lavatory"
-        actual = get_synonyms(word)
+        actual = self._language_service.get_synonyms(word)
         expected = ["toilet"]
         self.assertEqual(actual, expected)
 
     def test_get_synonyms_returns_empty_list_when_no_synonyms_found(self):
         word = "room"
-        actual = get_synonyms(word)
+        actual = self._language_service.get_synonyms(word)
         expected = []
         self.assertEqual(actual, expected)
